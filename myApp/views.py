97f6f2e42d27f87e1ee94d793c9f77ba33266b6c
rasc3ta <greenfair.shan5@gmail.com>
2025-09-17 06:45:35 +0600

    task done

--- myApp/views.py
+++ myApp/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from myApp.models import *
 
+
 def index(request):
-    
-    
-    
+
     return render(request, 'index.html', {
-        'students': Student.objects.all(),
+        'students': Student.objects.filter(age__gte=20),
         'teachers': Teacher.objects.all(),
         'employees': Employee.objects.all(),
     })
-    
